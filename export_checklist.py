51452cdaffce5d4c777a02fb202c7c4842ad8b8c
Ruud van Asseldonk <ruud@chorus.one>
2024-05-29 10:53:01 +0200

    Parse recommendations from the markdown files

--- export_checklist.py
+++ export_checklist.py
@@ -4,9 +4,22 @@
 export_checklist.py -- Export all recommendations as Markdown checklist.
 """
 
-from typing import Iterable
+from typing import Iterable, NamedTuple, Optional
 
 import os
+import sys
+
+
+class Recommendation(NamedTuple):
+    chapter: str
+    file: str
+    id: str
+    priority: int
+    title: str
+
+    def get_url(self) -> str:
+        slug = self.file.removeprefix("src/").removesuffix(".md")
+        return f"https://handbook.chorus.one/{slug}.html#{id}"
 
 
 def list_md_files() -> Iterable[str]:
@@ -16,9 +29,49 @@ def list_md_files() -> Iterable[str]:
                 yield os.path.join(root, file)
 
 
+def list_recommendations(fname: str) -> Iterable[Recommendation]:
+    chapter: Optional[str] = None
+
+    with open(fname, "r", encoding="utf-8") as f:
+        for i, line in enumerate(f):
+            if line.startswith("# "):
+                chapter = line[2:].strip()
+
+            if line.startswith("#### "):
+                try:
+                    # Recommendation lines are of the form `#### Title {.prio # #id}\n".
+                    title = line.removeprefix("#### ")
+                    title, meta = title.rsplit("{", maxsplit=1)
+                    meta = meta.strip().removesuffix("}")
+                    prio_str, id_str = meta.split(" ", maxsplit=1)
+                    assert prio_str.startswith(".p"), "Recommendation line must end in `{.pN #id}`."
+                    assert id_str.startswith("#"), "Recommendation line must end in `{.pN #id}`."
+                    assert chapter is not None, "Must have # chapter title before ####."
+
+                    yield Recommendation(
+                        chapter=chapter,
+                        file=fname,
+                        id=id_str[1:],
+                        priority=int(prio_str[2:]),
+                        title=title.strip(),
+                    )
+
+                except Exception as exc:
+                    print(f"Error in {fname} at line {i + 1}:")
+                    print(f"{i + 1} | {line}", end="")
+                    print(f"Error: {exc}")
+                    sys.exit(1)
+
+
 def main() -> None:
-    for f in list_md_files():
-        print(f)
+    for fname in list_md_files():
+        if fname == "src/node-software/intro.md":
+            # In the intro we list the priority categories, they are not
+            # themselves recommendations.
+            continue
+
+        for rec in list_recommendations(fname):
+            print(rec)
 
 
 if __name__ == "__main__":
